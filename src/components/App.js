8cc6c5a40a8fefabee5973da0578db052bc7dfbb
Felix Angell <felixangell@otzaf.org>
2021-05-25 20:57:41 +0100

    initial bit of styling and layout stuff

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,52 @@
-import './App.css';
+import styles from './App.module.css';
 import SearchBar from './SearchBar'
 import { useSelector } from 'react-redux'
 import { selectResults } from '../store'
 
-function App() {
-  const results = useSelector(selectResults)
+const SausageRating = ({ sausages }) => {
   return (
-    <div className="App">
-      <SearchBar />
+    <span>{new Array(parseInt(sausages || 0)).fill().map((item) => '🌭')}</span>
+  )
+}
+
+const ResultsContainer = () => {
+  const results = useSelector(selectResults)
+
+  return (
+    <div className={styles.ResultsContainer}>
       <table>
-      <thead>
-        <th>Name</th>
-        <th>Sausages</th>
-        <th>Ruffalos</th>
-      </thead>
-      <tbody>
-      {
-        results.map(({name, sausages, ruffalos}) => (
-          <tr key={name}>
-            <td>{name}</td>
-            <td>{sausages || "N/A"}</td>
-            <td>{ruffalos || "N/A"}</td>
-          </tr>
-        ))
-      }
-      </tbody>
+        <thead>
+          <th>Name</th>
+          <th>Sausages</th>
+          <th>Ruffalos</th>
+        </thead>
+
+        <tbody>
+          {
+            results.map(({ name, sausages, ruffalos }) => (
+              <tr key={name}>
+                <td>{name}</td>
+                <td>{<SausageRating sausages={sausages} /> || "N/A"}</td>
+                <td>{ruffalos || "N/A"}</td>
+              </tr>
+            ))
+          }
+        </tbody>
       </table>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <div className={styles.Logo}>
+        Sausearch
+      </div>
+
+      <SearchBar />
+
+      <ResultsContainer />
     </div>
   );
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { 
-    updateQueryFilter, 
-    updateMinSausagesFilter, 
-    updateMaxSausagesFilter, 
-    updateMinRuffalosFilter, 
+import {
+    updateQueryFilter,
+    updateMinSausagesFilter,
+    updateMaxSausagesFilter,
+    updateMinRuffalosFilter,
     updateMaxRuffalosFilter,
     updateIncludeSausageDisqualifiedFilter,
     updateIncludeWillItBlowDisqualifiedFilter,
@@ -12,24 +12,47 @@ import {
     resetFilters,
     selectFilters
 } from '../store'
+import styles from './SearchBar.module.css'
 
 const ScoreInput = (props) => (<input type="number" max="5" min="0" {...props} />)
 
-export default function SearchBar() {
+const FilteringOptions = ({ filters, dispathValueChangeEventFor }) => {
+    return (
+        <div className={styles.SearchBarFilteringContainer}>
+            <div>
+                Sausages: <ScoreInput value={filters.minSausages} onChange={dispathValueChangeEventFor(updateMinSausagesFilter)} /> to
+                <ScoreInput value={filters.maxSausages} onChange={dispathValueChangeEventFor(updateMaxSausagesFilter)} />
+            </div>
+
+            <span>Include disqualified <input type='checkbox'/></span>
+            
+            <div>
+                Ruffalos: <ScoreInput value={filters.minRuffalos} onChange={dispathValueChangeEventFor(updateMinRuffalosFilter)} /> to
+                <ScoreInput value={filters.maxRuffalos} onChange={dispathValueChangeEventFor(updateMaxRuffalosFilter)} />
+            </div>
+        </div>
+    )
+}
+
+const SearchBar = () => {
     const filters = useSelector(selectFilters)
     const dispatch = useDispatch()
     const withUpdateEvent = (callback) => e => callback(e.target.value) && console.log(e.target.value)
     const dispathValueChangeEventFor = (actionGenerator) => withUpdateEvent(val => dispatch(actionGenerator(val)))
+    const [visible, setVisible] = useState(false)
 
     return (
-        <div>
+        <div className={styles.SearchBarContainer}>
             <input type="text" placeholder="Search" value={filters.query} onChange={dispathValueChangeEventFor(updateQueryFilter)} />
-            Sausages: <ScoreInput value={filters.minSausages} onChange={dispathValueChangeEventFor(updateMinSausagesFilter)} /> to 
-            <ScoreInput value={filters.maxSausages} onChange={dispathValueChangeEventFor(updateMaxSausagesFilter)} /> (
-            Include disqualified - checkbox placeholder -)
-            Ruffalos: <ScoreInput value={filters.minRuffalos} onChange={dispathValueChangeEventFor(updateMinRuffalosFilter)} /> to 
-            <ScoreInput value={filters.maxRuffalos} onChange={dispathValueChangeEventFor(updateMaxRuffalosFilter)} />
+
+            <>
+                <button onClick={() => setVisible(!visible)}>Show { visible ? 'Less' : 'More' }</button>
+                {visible && <FilteringOptions filters={filters} dispathValueChangeEventFor={dispathValueChangeEventFor} />}
+            </>
+
             <input type="button" value="Reset" onClick={() => dispatch(resetFilters)} />
         </div>
     )
 }
+
+export default SearchBar
